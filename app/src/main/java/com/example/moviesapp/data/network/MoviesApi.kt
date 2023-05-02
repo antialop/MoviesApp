ae5ff75454097734e30c6281a78e0aec232b34f0
antialopez <antialopezgarcia@gmail.com>
2023-05-02 16:17:39 +0200

    get popular movies

--- app/src/main/java/com/example/moviesapp/data/network/MoviesApi.kt
+++ app/src/main/java/com/example/moviesapp/data/network/MoviesApi.kt
@@ -1,4 +1,13 @@
 package com.example.moviesapp.data.network
 
+import com.example.moviesapp.data.network.model.PopularMovieResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface MoviesApi {
+    @GET("movie/popular")
+    fun getSuperheroes(
+        @Query("api_key") apiKey: String,
+    ): Call<PopularMovieResponse>
 }
