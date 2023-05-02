0403b58133e6d555b08055e28822275553c749c7
antialopez <antialopezgarcia@gmail.com>
2023-05-02 13:02:48 +0200

    connect viewmodel fragment

--- app/src/main/java/com/example/moviesapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/moviesapp/ui/home/HomeFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.moviesapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
+    private val viewModel by viewModels<HomeViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/example/moviesapp/ui/watchlist/WatchListFragment.kt
+++ app/src/main/java/com/example/moviesapp/ui/watchlist/WatchListFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.moviesapp.R
+import com.example.moviesapp.ui.home.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class WatchListFragment : Fragment() {
-
+    private val viewModel by viewModels<WatchlistViewModel>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
