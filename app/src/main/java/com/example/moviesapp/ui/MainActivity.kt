21e3252ff2d8d70cdbca8abac473df56275a0c2e
antialopez <antialopezgarcia@gmail.com>
2023-05-02 11:29:46 +0200

    navigation completed

--- app/src/main/java/com/example/moviesapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/ui/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.moviesapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.moviesapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottonNavigationView)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
