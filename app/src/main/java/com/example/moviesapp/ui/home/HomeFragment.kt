3cac0e84a3fb3931064aa6234e1428ae6e0a35e8
antialopez <antialopezgarcia@gmail.com>
2023-05-02 12:46:14 +0200

    AndroidEntryPoint main and fragments

--- app/src/main/java/com/example/moviesapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/moviesapp/ui/home/HomeFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.moviesapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
--- app/src/main/java/com/example/moviesapp/ui/watchlist/WatchListFragment.kt
+++ app/src/main/java/com/example/moviesapp/ui/watchlist/WatchListFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.moviesapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class WatchListFragment : Fragment() {
 
     override fun onCreateView(
