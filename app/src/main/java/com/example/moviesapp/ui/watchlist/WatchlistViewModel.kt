61a5d72e1e15555b85b1579fa6c3d27460e9af61
antialopez <antialopezgarcia@gmail.com>
2023-05-02 12:59:12 +0200

    create viewModels fragments and inject

---
+++ app/src/main/java/com/example/moviesapp/ui/watchlist/WatchlistViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.moviesapp.ui.watchlist
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class WatchlistViewModel @Inject constructor():ViewModel() {
+}
