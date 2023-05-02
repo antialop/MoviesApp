545876bc823dfee67d00337543f2b2247eb51b7a
antialopez <antialopezgarcia@gmail.com>
2023-05-02 12:43:41 +0200

    fix application HiltAndroidApp

--- app/src/main/java/com/example/moviesapp/ui/MoviesApp.kt
+++ app/src/main/java/com/example/moviesapp/ui/MoviesApp.kt
@@ -2,7 +2,8 @@ package com.example.moviesapp.ui
 
 import android.app.Application
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
-@AndroidEntryPoint
+@HiltAndroidApp
 class MoviesApp:Application() {
 }
