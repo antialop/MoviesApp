6e06ec866234e8b2d696563e619a3d4fe7fb8ec4
antialopez <antialopezgarcia@gmail.com>
2023-05-05 13:09:55 +0200

    create watchlist dao

---
+++ app/src/main/java/com/example/moviesapp/data/database/dao/WatchlistMovieDao.kt
@@ -0,0 +1,16 @@
+package com.example.moviesapp.data.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.moviesapp.data.database.entities.WatchlistMovieEntity
+
+@Dao
+interface WatchlistMovieDao {
+    @Query("SELECT * FROM watchlist_table")
+    suspend fun getAllWatchlistMovies(): List<WatchlistMovieEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertWatchlistMovies(watchlistMovieEntity: WatchlistMovieEntity)
+}
